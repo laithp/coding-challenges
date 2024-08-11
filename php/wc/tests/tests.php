1b089be92020f93166b5cd51f67de1bb45e40c4d
Lathrop Preston <lathrop@prestonfam.org>
2024-08-10 19:06:47 -0500

    basic tests - fail right now - may have solution to my phpunit problem

--- php/wc/tests/tests.php
+++ php/wc/tests/tests.php
@@ -3,4 +3,15 @@
  * I'm having issues getting phpunit setup in my current evironment, so I'm doing a freeform unit testing setup
  */
 
- 
+require('unit.class.php');
+
+require('./ccwc.class.php');
+
+$unittest = new UnitTest('CCWC');
+
+$ccwc = new CCWC();
+
+$startcount = 0;
+$teststring = "abcdgefghigklmnop0987654321!";
+$result = $ccwc->get_bytecount($startcount, $teststring); 
+if($result==27){ echo 'pass'; }else{ echo 'fail'; }
