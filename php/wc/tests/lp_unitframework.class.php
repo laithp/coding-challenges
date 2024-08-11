ab369ec9830ddc87b23e259acee454fd42df9eeb
Lathrop Preston <lathrop@prestonfam.org>
2024-08-10 19:58:49 -0500

    building unit tests

---
+++ php/wc/tests/lp_unitframework.class.php
@@ -0,0 +1,46 @@
+<?php
+
+class UnitFramework
+{
+
+    public $classname = null;
+    public $testcount = 0;
+    public $pass = 0;
+    public $fail = 0;
+
+    public $testdescription = "";
+
+    function __construct($name)
+    {
+        $this->classname = $name;
+    }
+
+    function testDescription($description)
+    {
+        $this->testdescription = $description;
+    }
+
+    function testPass()
+    {
+        $this->pass++;
+        echo $this->testdescription . "\n";
+        echo "PASS!\n\n";
+    }
+    function testFail($message)
+    {
+        $this->fail++;
+        echo $this->testdescription . "\n";
+        echo "FAIL!\n";
+        echo $message;
+    }
+
+    function assertEqual($expected, $actual)
+    {
+        $this->testcount++;
+        if ($expected == $actual) {
+            $this->testPass();
+        } else {
+            $this->testFail("Expected: " . $expected . "\nActual: " . $actual . "\n\n");
+        }
+    }
+}
